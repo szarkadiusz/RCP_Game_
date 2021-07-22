40b26458bdc4da8a88cc9d3ff7f5f1752b24f4f2
szarkadiusz <arkadiuszsz@gmail.com>
2021-07-22 12:50:43 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import ScreenManager,FadeTransition
+from kivy.uix.image import Image
+from random import randint as r
+class RSP(MDApp):
+
+    gameOption={1:'paper.png',
+                2:'rock.png',
+                3:'scissors.png'}
+
+    randomNum=0
+    def build(self):
+        self.theme_cls.primary_palette = "Amber"
+
+
+    def addImage(self):
+        randomNum=r(1,3)
+
+
+
+        self.root.ids.game_image.source=RSP.gameOption.get(randomNum)
+
+#TODO może transition inne ?
+
+if __name__ == '__main__':
+    RSP().run()
